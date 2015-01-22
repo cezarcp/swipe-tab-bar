640cd48fe29baa9f7550412ae63f33b7c0145c36
Cezar <contact@cezar.co>
2015-01-22 16:21:03 -0300

    Initial Commit

---
+++ SwipeTabBar/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  SwipeTabBar
+//
+//  Created by Cezar Carvalho Pereira on 22/1/15.
+//  Copyright (c) 2015 Wavebits. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SwipeTabBar/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SwipeTabBar
+//
+//  Created by Cezar Carvalho Pereira on 22/1/15.
+//  Copyright (c) 2015 Wavebits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
