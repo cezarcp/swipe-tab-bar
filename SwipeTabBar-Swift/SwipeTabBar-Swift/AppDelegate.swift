07e1d7fad469e6f83466eea1b420212a31e15d1a
kirankunigiri <kirankunigiri@gmail.com>
2016-07-12 12:01:44 -0700

    Updated Swift syntax and Storyboard UI
    I updated Swift syntax to latest version. The Storyboard also had all
    views with a title of “First View,” so I numbered them.

--- SwipeTabBar-Swift/SwipeTabBar-Swift/AppDelegate.swift
+++ SwipeTabBar-Swift/SwipeTabBar-Swift/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let tabBarController = self.window?.rootViewController as? UITabBarController
-        tabBarController?.setupSwipeGestureRecognizers(cycleThroughTabs: true)
+        tabBarController?.setupSwipeGestureRecognizers(true)
         
         return true
     }
